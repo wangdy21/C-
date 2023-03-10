7f9e86d5ee002c22d86cff914ce08f69c9e2160d
wdy <wangdy21@mails.com>
2023-03-10 11:44:21 +0800

    如果一个类中含有虚函数，则其析构函数要用虚函数

--- test.cpp
+++ test.cpp
@@ -1,2 +1,27 @@
-ppp
-dfjksjhfkj
+#include<iostream>
+
+class Base{
+public:
+     Base(){
+
+     }
+     virtual ~Base(){
+        std::cout<<"Base" << std::endl;
+     }
+};
+
+class Derived:public Base {
+public:
+     Derived(){
+
+     }
+     ~Derived(){
+        std::cout<<"Derived" << std::endl;
+     }
+};
+
+int main(){
+    Base *b = new Derived();
+    delete b;
+    return 0;
+}
